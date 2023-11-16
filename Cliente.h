9385e4ac16c5555ad9b9ab7e8e137fb654e77d4c
lorainvglezr <149640984+lorainvglezr@users.noreply.github.com>
2023-11-16 05:43:29 -0600

    Cliente.h

--- Cliente.h
+++ Cliente.h
@@ -1,68 +1,43 @@
 // Nombre: Lorain Valeria González Rivas
 // Matrícula: A01707279
-// Fecha: Miércoles, 8 de noviembre de 2023
+// Fecha: Miércoles, 15 de noviembre de 2023
 // Materia: Pensamiento Computacional Orientado a Objetos
 
 /**
- * Clase Cliente, en esta clase se establecen las variables que son características
- * del cliente que está en el restaurante, maneja los datos personales de
+ * Clase Cliente, en esta clase se establecen las variables que son características sólo
+ * del cliente que está en el restaurante, maneja los datos personales financieros de
  * la persona que entra al restaurante.
  */
 
 #include <string>
+#include "Personas.h"
 
-class Cliente{
+class Cliente: public Personas{
 
 private:
-    //Declaración de las variables de instancia
-    std::string nombre;
-    int edad;
-    std::string genero;
+    //Declaración de las variables de instancia, propias de esta clase
     float disp_dinero;
 
 public:
-    //Constuctor
-    Cliente():nombre(""), edad(0), genero(""), disp_dinero(0){};
-    Cliente(std::string nom, int ed, std::string gen, float dis_din): nombre(nom), edad(ed), genero(gen), disp_dinero(dis_din){};
+    //Constuctor, en donde se manda a llamar a la clase base
+    Cliente(): Personas(), disp_dinero(0){};
+    Cliente(std::string nom, int ed, std::string gen, float dis_din): Personas(nom, ed, gen), disp_dinero(dis_din){};
 
 
     // Métodos miembros de la clase
 
     // get variables
-    std::string get_nombre();
-    int get_edad();
-    std::string get_genero();
     float get_disp_dinero();
 
     //set variables
-    void set_nombre(std::string);
-    void set_edad(int);
-    void set_genero(std::string);
     void set_disp_dinero(float);
 };
 
-std::string Cliente::get_nombre(){
-    return nombre;
-}
-int Cliente::get_edad(){
-    return edad;
-}
-std::string Cliente::get_genero(){
-    return genero;
-}
+
 float Cliente::get_disp_dinero(){
     return disp_dinero;
 }
 
-void Cliente::set_nombre(std::string nom){
-    nombre = nom;
-}
-void Cliente::set_edad(int ed){
-    edad = ed;
-}
-void Cliente::set_genero(std::string gen){
-    genero = gen;
-}
 void Cliente::set_disp_dinero(float dis_din){
     disp_dinero = dis_din;
 }
