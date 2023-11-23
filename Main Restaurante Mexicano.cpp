42bc726ea6bc39960aa4c9a4786f4e6f3b2c6fed
lorainvglezr <149640984+lorainvglezr@users.noreply.github.com>
2023-11-23 08:46:27 -0600

    Main Restaurante Mexicano.cpp

--- Main Restaurante Mexicano.cpp
+++ Main Restaurante Mexicano.cpp
@@ -1,6 +1,6 @@
 // Nombre: Lorain Valeria González Rivas
 // Matrícula: A01707279
-// Fecha: Miércoles, 15 de noviembre de 2023
+// Fecha: Miércoles, 22 de noviembre de 2023
 // Materia: Pensamiento Computacional Orientado a Objetos
 
 // Descripción este es un proyecto demo para la clase de Pensamiento Computacional Orientado a Objetos
@@ -11,9 +11,11 @@
 // Bibliotecas
 #include <iostream>
 #include <string>
-#include "Platillo.h"
 #include "Cliente.h"
+#include "Platillo.h"
 #include "Mesero.h"
+#include "Factura.h"
+
 
 // Función principal
 int main(){
@@ -39,7 +41,7 @@ int main(){
     std::cout << std::endl;
 
     //Uso de la clase Cliente, en donde se establecen los valores de la variables
-    // y se hace uso de las funciones de los documentos .h (Personas y Cliente)
+    // y se hace uso de las funciones de los documentos .h
     Cliente cliente_1("Leisa", 20, "Mujer",40357.86);
     std::cout << "El nombre del cliente es: ";
     std::cout << cliente_1.get_nombre() << std::endl;
@@ -52,8 +54,8 @@ int main(){
 
     std::cout << std::endl;
 
-    // Uso de la clase Mesero, en donde se establecen los valores de la variables
-    // y se hace uso de las funciones de los documentos .h (Personas y Mesero)
+    // Uso de la clase Platillo, en donde se establecen los valores de la variables
+    // y se hace uso de las funciones del documento .h
     Mesero mesero_1("Lorenzo", 27, "Hombre", 15.6, 43050.85);
     std::cout << "El nombre del mesero es: ";
     std::cout << mesero_1.get_nombre() << std::endl;
@@ -67,6 +69,13 @@ int main(){
     std::cout << mesero_1.get_salario() << std::endl;
 
     std::cout << std::endl;
+
+    // Uso de funciones de la clase Factura
+    Factura factura1(cliente_1);
+    factura1.agregarPlatillo(platillo_1);
+    factura1.mostrarFactura();
+
+    return 0;
 
     //Despliegue de un mensaje de despedida al usuario
     std::cout << "Esos fueron los datos de su estancia en 'La casona de la abuela' " << std::endl;
