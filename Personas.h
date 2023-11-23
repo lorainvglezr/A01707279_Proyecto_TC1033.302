49e0cc0099b5c1a1712cfd02772dd209fc0046c9
lorainvglezr <149640984+lorainvglezr@users.noreply.github.com>
2023-11-23 08:48:40 -0600

    Personas.h

--- Personas.h
+++ Personas.h
@@ -1,6 +1,6 @@
 // Nombre: Lorain Valeria González Rivas
 // Matrícula: A01707279
-// Fecha: Miércoles, 15 de noviembre de 2023
+// Fecha: Miércoles, 22 de noviembre de 2023
 // Materia: Pensamiento Computacional Orientado a Objetos
 
 /**
@@ -8,34 +8,35 @@
  * un individuo en general, el cual puede ser un cliente o un mesero (clases hijas).
  */
 
+#pragma once
 #include <string>
 
 class Personas{
-    
+
 private:
     //Declaración de las variables de instancia generales para clases hijas
     std::string nombre;
     int edad;
     std::string genero;
-    
+
 public:
     //Constuctor
     Personas(): nombre(""), edad(0), genero(""){};
     Personas(std::string nom, int ed, std::string gen):nombre(nom), edad(ed), genero(gen){};
-    
-    
+
+
     // Métodos miembros de la clase
-    
+
     // get variables
     std::string get_nombre();
     int get_edad();
     std::string get_genero();
-    
+
     // set variables
     void set_nombre(std::string);
     void set_edad(int);
     void set_genero(std::string);
-    
+
 };
 
 
